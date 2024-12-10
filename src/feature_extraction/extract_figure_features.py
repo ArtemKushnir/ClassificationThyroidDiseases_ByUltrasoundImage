6ca262a3c095557ddd8ebfc86ee809f60cae3303
ArtemKushnir <artem.kushnir.79@gmail.com>
2024-12-10 08:30:12 +0300

    feat: collect more features(fourier, wavelet)

--- src/feature_extraction/extract_figure_features.py
+++ src/feature_extraction/extract_figure_features.py
@@ -4,13 +4,16 @@ import xml.etree.ElementTree as ET
 
 import cv2 as cv
 import numpy as np
+import pywt
 from scipy.stats import kurtosis, skew
 from skimage.feature import graycomatrix, graycoprops
 
 from src.utils.get_paths import get_join_paths, get_paths_with_extension
 
-FOLDER_CLEAN_IMAGE_PATH = "../data/clear_and_cropped_image_data/"
-FOLDER_IMAGE_PATH = "../data/image_data/"
+FOLDER_CLEAN_IMAGE_PATH = (
+    "/home/kush/machine_learning/ClassificationThyroidDiseases_ByUltrasoundImage/data/clear_and_cropped_image_data/"
+)
+FOLDER_IMAGE_PATH = "/home/kush/machine_learning/ClassificationThyroidDiseases_ByUltrasoundImage/data/image_data/"
 
 
 def get_points_from_images(folder_image_path=FOLDER_CLEAN_IMAGE_PATH, folder_xml_path=FOLDER_IMAGE_PATH):
@@ -42,6 +45,64 @@ def get_points_from_images(folder_image_path=FOLDER_CLEAN_IMAGE_PATH, folder_xml
     return figures
 
 
+def get_wavelet_features(image, mask):
+    result = {}
+
+    masked_image = image.copy()
+    masked_image[mask != 255] = 0
+
+    coeffs2 = pywt.dwt2(masked_image, "bior1.3")
+    ll, (lh, hl, hh) = coeffs2
+
+    wavelet_stats = {
+        "LL_mean": np.mean(ll),
+        "LH_mean": np.mean(lh),
+        "HL_mean": np.mean(hl),
+        "HH_mean": np.mean(hh),
+        "LL_std": np.std(ll),
+        "LH_std": np.std(lh),
+        "HL_std": np.std(hl),
+        "HH_std": np.std(hh),
+        "LL_energy": np.sum(ll**2),
+        "LH_energy": np.sum(lh**2),
+        "HL_energy": np.sum(hl**2),
+        "HH_energy": np.sum(hh**2),
+    }
+    for name, stat in wavelet_stats.items():
+        result[f"figure_{name}"] = stat
+
+    return result
+
+
+def get_fourier_features(image, mask):
+    result = {}
+
+    masked_image = image.copy()
+    masked_image[mask != 255] = 0
+
+    f_transform = np.fft.fft2(masked_image)
+
+    f_transform_shifted = np.fft.fftshift(f_transform)
+
+    magnitude_spectrum = np.abs(f_transform_shifted)
+
+    phase_spectrum = np.angle(f_transform_shifted)
+
+    fourier_stats = {
+        "mean_magnitude": np.mean(magnitude_spectrum),
+        "std_magnitude": np.std(magnitude_spectrum),
+        "max_magnitude": np.max(magnitude_spectrum),
+        "min_magnitude": np.min(magnitude_spectrum),
+        "mean_phase": np.mean(phase_spectrum),
+        "std_phase": np.std(phase_spectrum),
+    }
+
+    for name, stat in fourier_stats.items():
+        result[f"figure_{name}"] = stat
+
+    return result
+
+
 def process_json(json_data):
     result = []
     data = json.loads(json_data)
@@ -50,7 +111,7 @@ def process_json(json_data):
     return result
 
 
-def glcm_matrix_features(mask, image):
+def get_glcm_matrix_features(mask, image):
     result = {}
     masked_image = image.copy()
     masked_image[mask != 255] = 0
@@ -67,12 +128,12 @@ def glcm_matrix_features(mask, image):
     glcm = graycomatrix(processed_image, distances, angles, levels=16, symmetric=True, normed=True)
 
     glcm_stats = {
-        "contrast": graycoprops(glcm, "contrast"),
-        "dissimilarity": graycoprops(glcm, "dissimilarity"),
-        "homogeneity": graycoprops(glcm, "homogeneity"),
-        "asm": graycoprops(glcm, "ASM"),
-        "energy": np.sqrt(graycoprops(glcm, "ASM")),
-        "correlation": graycoprops(glcm, "correlation"),
+        "glcm_contrast": graycoprops(glcm, "contrast"),
+        "glcm_dissimilarity": graycoprops(glcm, "dissimilarity"),
+        "glcm_homogeneity": graycoprops(glcm, "homogeneity"),
+        "glcm_asm": graycoprops(glcm, "ASM"),
+        "glcm_energy": np.sqrt(graycoprops(glcm, "ASM")),
+        "glcm_correlation": graycoprops(glcm, "correlation"),
     }
     for name, stat in glcm_stats.items():
         result[f"figure_0_{name}"] = stat[0][0]
@@ -93,7 +154,11 @@ def get_feature(image, points_array):
     mask = np.zeros(image_size, dtype=np.uint8)
     cv.fillPoly(mask, [polygon], 255)
 
-    result.update(glcm_matrix_features(mask, image))
+    result.update(get_glcm_matrix_features(mask, image))
+
+    result.update(get_fourier_features(image, mask))
+
+    result.update(get_wavelet_features(image, mask))
 
     area = cv.countNonZero(mask)
     intensity_values = image[mask == 255]
